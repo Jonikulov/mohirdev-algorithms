f00ed6fe8aabfc3bb4d741fdfcb1cc6d2c8188ea
Javohir <javohir200211@gmail.com>
2021-12-02 18:45:34 +0500

    modified binarySearch

--- lesson-02-search-algorithms/binarySearch.py
+++ lesson-02-search-algorithms/binarySearch.py
@@ -5,7 +5,7 @@ myList = [1,3,4,6,7,8,10,12,23,45,56,78,99]
 def binarySearch(given_list,item):
     low = 0
     high = len(given_list)-1
-    while low<high:
+    while low<=high:
         mid = int((low+high)/2)
         if given_list[mid]==item:
             return mid
@@ -21,3 +21,4 @@ print(binarySearch(myList, 10))
 print(binarySearch(myList, 45))
 print(binarySearch(myList, 11))
 print(binarySearch(myList, 100))
+print(binarySearch(myList, 99))
