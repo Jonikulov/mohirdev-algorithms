7450c50db02c7c2f889b5a8f06f3daacecca8bfb
Javohir <javohir200211@gmail.com>
2021-12-06 22:10:55 +0500

    added main

---
+++ lesson-04-array-and-linked-list/main.py
@@ -0,0 +1,39 @@
+
+from linkedLists import Node, LinkedList
+
+# creating a new linked list
+llist = LinkedList()
+
+# assigning nodes
+llist.head = Node("work-out")
+read = Node("reading")
+classes = Node("classes")
+cycle = Node("cycling")
+study = Node("courses")
+practice = Node("coding")
+
+# linking nodes
+llist.head.next = read
+read.next = classes
+classes.next = cycle
+cycle.next = study
+study.next = practice
+
+# print(llist.head.data)
+# print(llist.head.next.data)
+# print(read.next.data)
+
+llist.printList()
+print()
+
+# llist.push('running')
+# llist.printList()
+
+# llist.insertAfter(llist.head.next,'science')
+# llist.printList()
+
+# llist.append('maths')
+# llist.printList()
+
+llist.deleteNode('coding')
+llist.printList()
