72eabb5a13dfe05560932ca510bdbf2e1e09c6d5
Javohir <javohir200211@gmail.com>
2021-12-09 17:52:08 +0500

    modified selectionSort

--- lesson-05-selection-sort/selectionSort.py
+++ lesson-05-selection-sort/selectionSort.py
@@ -4,15 +4,15 @@ def selectionSort(array):
     sorted_list = []
 
     while array:
-        max_item = array[0]
-        for k in array:
-            if k>max_item:
-                max_item = k
-        sorted_list.append(max_item)
-        # need an improvement
-        array.remove(max_item)
+        max_item = 0
+
+        for i in range(len(array)):
+            if array[i]>array[max_item]:
+                max_item = i
+
+        sorted_list.append(array.pop(max_item))
     return sorted_list
 
-list1 = [3, 41, 999, 5, 132,33,33,33, 6, 7, 8, 9, 34, 367, 92, 821, 281]
 
+list1 = [3, 41, 999, 5, 132,33,33,33, 6, 7, 8, 9, 34, 367, 92, 821, 281]
 print(selectionSort(list1))
