bf2be416bf3316aa593d3d9edc4bc648a9093cc2
Javohir <javohir200211@gmail.com>
2021-12-12 11:43:35 +0500

    added recursion

---
+++ lesson-06-recursion-and-stack/recursion.py
@@ -0,0 +1,18 @@
+# calculating n factorial
+def facto(n):
+    i=1
+    while n>0:
+        i = i*n
+        n-=1
+    return i
+
+print(facto(5))
+
+# N factorial with recursion
+def factorial(N):
+    if N>1:
+        return N*factorial(N-1)
+    else:
+        return 1
+
+print(factorial(5))
