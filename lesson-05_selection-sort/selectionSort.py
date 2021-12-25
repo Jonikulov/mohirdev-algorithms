6b66fc7a950548213b8ed9a3bae35f286d172c00
Javohir <javohir200211@gmail.com>
2021-12-25 19:43:43 +0500

    added lesson 05

---
+++ lesson-05_selection-sort/selectionSort.py
@@ -0,0 +1,18 @@
+# Selection Sort Algorithm
+
+def selectionSort(array):
+    sorted_list = []
+
+    while array:
+        max_item = 0
+
+        for i in range(len(array)):
+            if array[i]>array[max_item]:
+                max_item = i
+
+        sorted_list.append(array.pop(max_item))
+    return sorted_list
+
+
+list1 = [3, 41, 999, 5, 132, 33, 33, 33, 6, 7, 8, 9, 34, 367, 92, 821, 281]
+print(selectionSort(list1))
