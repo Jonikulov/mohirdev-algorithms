3deb2acf847465b0fb9b00229fece22b8f5ee8b7
Javohir <javohir200211@gmail.com>
2021-12-26 11:52:48 +0500

    added div_conq_problems

---
+++ lesson_07-divide_&_conquer-quicksort/div_conq_problems.py
@@ -0,0 +1,11 @@
+def len_array():
+    """returns the number of array elements"""
+    pass
+
+def max_array():
+    """returns the largest element of an array"""
+    pass
+
+def binary_search():
+    """Binary Search Search Algorithm"""
+    pass
