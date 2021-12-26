639faaa4721571bc9357305579cc10f2150b9fb1
Javohir <javohir200211@gmail.com>
2021-12-26 11:42:33 +0500

    modified euclideanAlgorithm

--- lesson_07-divide_&_conquer-quicksort/euclideanAlgorithm.py
+++ lesson_07-divide_&_conquer-quicksort/euclideanAlgorithm.py
@@ -17,3 +17,9 @@ print(euclideanAlgorithm(45,27)) # -> 9
 
 print(euclideanAlgorithm(500,1500)) # -> 500
 print(euclideanAlgorithm(27,45)) # -> 9
+
+# answer: with recursion
+def gcd(a, b):
+    if a == 0 :
+        return b      
+    return gcd(b%a, a)
