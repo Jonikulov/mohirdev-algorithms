84417a4e3ee0a554e6505bf591939d91f1f251ea
Javohir <javohir200211@gmail.com>
2021-12-30 09:45:48 +0500

    modified div_conq_problems

--- lesson_07-divide_&_conquer-quicksort/div_conq_problems.py
+++ lesson_07-divide_&_conquer-quicksort/div_conq_problems.py
@@ -15,16 +15,19 @@ def max_array(array):
 def binary_search_recursive(arr, elem, low=0, high=None):
     if high == None:
         high = len(arr) - 1
-    if low > high:
-        return None
 
-    mid = (low + high) // 2
-    if elem == arr[mid]:
-        return mid
-    if elem < arr[mid]:
-        return binary_search_recursive(arr, elem, low, mid-1)
-    # elem > arr[mid]
-    return binary_search_recursive(arr, elem, mid+1, high)
+    if low <= high:
+        mid = (low + high) // 2
+        if elem == arr[mid]:
+            return mid
+        elif elem < arr[mid]:
+            return binary_search_recursive(arr, elem, low, mid-1)
+        else:
+        # elem > arr[mid]
+            return binary_search_recursive(arr, elem, mid+1, high)
+    else:
+        # if element not in the list
+        return None
 
 
 array = [2,4,5,6,8,9,10,11,12,15,25,37,40]
