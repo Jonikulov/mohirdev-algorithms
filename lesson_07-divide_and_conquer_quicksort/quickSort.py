434c2f4536296337b6c9f3340ee5ec10625accbe
Javohir <javohir200211@gmail.com>
2022-04-21 21:06:59 +0500

    modified quicksort

--- lesson_07-divide_and_conquer_quicksort/quickSort.py
+++ lesson_07-divide_and_conquer_quicksort/quickSort.py
@@ -7,8 +7,10 @@ def quickSort(arr):
         pivot = arr.pop(randrange(len(arr)))
         low = [i for i in arr if i<=pivot]
         high = [i for i in arr if i>pivot]
+        print(f"\npivot: {pivot}")
+        print(f"low: {low}")
+        print(f"high: {high}")
         return quickSort(low) + [pivot] + quickSort(high)
-
 
 array = [3, 41, 999, 5, 132, 33, 33, 33, 6, 7, 8, 9, 34, 367, 92, 821, 281]
 print(quickSort(array))
